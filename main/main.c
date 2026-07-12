c6e176125f38eb0fad6f0644e1f8912280c13d35
ImageSphere-official <federicostaana02@gmail.com>
2026-07-13 00:45:47 +0800

    testers

--- main/main.c
+++ main/main.c
@@ -1,53 +1,15 @@
-#include <string.h>
-#include "freertos/FreeRTOS.h"
-#include "esp_wifi.h"
-#include "esp_event.h"
+#include <stdio.h>
 #include "esp_log.h"
-#include "nvs_flash.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
 
-#define ESP_WIFI_SSID      "ESP32WIFI5G"
-#define ESP_WIFI_PASS      "2519eredmia3"
-#define ESP_WIFI_CHANNEL   1
-#define MAX_STA_CONN       4
-
-static const char *TAG = "wifi softAP";
-
-void wifi_init_softap(void)
-{
-    ESP_ERROR_CHECK(esp_netif_init());
-    ESP_ERROR_CHECK(esp_event_loop_create_default());
-    esp_netif_create_default_wifi_ap();
-
-    wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
-    ESP_ERROR_CHECK(esp_wifi_init(&cfg));
-
-    wifi_config_t wifi_config = {
-        .ap = {
-            .ssid = ESP_WIFI_SSID,
-            .ssid_len = strlen(ESP_WIFI_SSID),
-            .channel = ESP_WIFI_CHANNEL,
-            .password = ESP_WIFI_PASS,
-            .max_connection = MAX_STA_CONN,
-            .authmode = WIFI_AUTH_WPA2_PSK
-        },
-    };
-
-    ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_AP));
-    ESP_ERROR_CHECK(esp_wifi_set_config(WIFI_IF_AP, &wifi_config));
-    ESP_ERROR_CHECK(esp_wifi_start());
-
-    ESP_LOGI(TAG, "wifi_init_softap finished. SSID:%s password:%s", ESP_WIFI_SSID, ESP_WIFI_PASS);
-}
+static const char *TAG = "HELLO_WORLD";
 
 void app_main(void)
 {
-    esp_err_t ret = nvs_flash_init();
-    if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
-        ESP_ERROR_CHECK(nvs_flash_erase());
-        ret = nvs_flash_init();
+    while(1) {
+        ESP_LOGI(TAG, "Hello, world!");
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
     }
-    ESP_ERROR_CHECK(ret);
-
-    wifi_init_softap();
 }
 
